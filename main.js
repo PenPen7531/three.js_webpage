b02f34856d378d4ced907debb67e2f62022a7be3
Jeff7531 <jeffmkwang@gmail.com>
2022-08-18 13:59:28 -0700

    changes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css"
 
 import * as THREE from 'three';
 
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75 , window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -50,7 +50,7 @@ const gridHelper = new THREE.GridHelper(200, 50);
 
 
 
-
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const cometBackground = new THREE.TextureLoader().load("comet.jpg");
 function addStar(){
